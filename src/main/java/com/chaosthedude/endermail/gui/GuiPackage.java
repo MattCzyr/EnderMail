c6ae1426fc0dc9fca81a586cd67640c346895206
mattczyr <mattczyr@gmail.com>
2018-08-14 23:50:08 -0400

    Fix missing background and tooltip rendering in GuiPackage

--- src/main/java/com/chaosthedude/endermail/gui/GuiPackage.java
+++ src/main/java/com/chaosthedude/endermail/gui/GuiPackage.java
@@ -30,6 +30,13 @@ public class GuiPackage extends GuiContainer {
 	}
 
 	@Override
+	public void drawScreen(int mouseX, int mouseY, float partialTicks) {
+        drawDefaultBackground();
+        super.drawScreen(mouseX, mouseY, partialTicks);
+        renderHoveredToolTip(mouseX, mouseY);
+    }
+
+	@Override
 	protected void drawGuiContainerForegroundLayer(int mouseX, int mouseY) {
 		fontRenderer.drawString(I18n.format(packageInventory.getName()), 8, 6, 4210752);
 		fontRenderer.drawString(playerInventory.getDisplayName().getUnformattedText(), 8, ySize - 96 + 2, 4210752);
