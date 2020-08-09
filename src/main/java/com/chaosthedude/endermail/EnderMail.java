46c5801713438ea958b7b779a1d0324addaad66a
Matt Czyr <mattczyr@gmail.com>
2020-08-09 17:12:53 -0400

    Bump version and update changelog

--- src/main/java/com/chaosthedude/endermail/EnderMail.java
+++ src/main/java/com/chaosthedude/endermail/EnderMail.java
@@ -43,7 +43,7 @@ public class EnderMail {
 
 	public static final String MODID = "endermail";
 	public static final String NAME = "Ender Mail";
-	public static final String VERSION = "1.1.2";
+	public static final String VERSION = "1.1.3";
 
 	public static final Logger logger = LogManager.getLogger(MODID);
 
